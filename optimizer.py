4303423dc0dcb8897562b1c3a793e0bce36fc905
Tislander1 <blurryprojection@gmail.com>
2025-04-17 22:11:24 -0600

    Update basic algorithm.  Need to add logging for function evaluations later.

--- optimizer.py
+++ optimizer.py
@@ -1,34 +1,71 @@
 import scipy.optimize as opt
 import numpy as np
 from functools import partial
-from time import sleep
+from time import sleep, time
+import multiprocessing
 
-evaluation_log = []
 
-def eggholder(vec):
-    sleep(0.1) # Simulate a time-consuming function
-    print(f"Evaluating {vec}")
+def myfunc(vec):
+    sleep(0.2) #Simulate a time-consuming function
+    # print(f"Evaluating {vec}")
     return (-(vec[1] + 47.0)
             * np.sin(np.sqrt(abs(vec[0]/2.0 + (vec[1] + 47.0))))
             - vec[0] * np.sin(np.sqrt(abs(vec[0] - (vec[1] + 47.0))))
             )
 
-def logged_function(func, vec):
-    value = func(vec)
-    evaluation_log.append((vec, value))  # Log the input and output
-    return value
+class get_time:
+    """A context manager to measure the execution time of a block of code."""
+    def __init__ (self):
+        self.start = time()
+        self.previous = time()
 
-bounds = [(-512, 512), (-512, 512)]
-function_name = 'eggholder'
+    def delta(self):
+        self.end = time()
+        self.interval = self.end - self.start
+        self.delta = self.end - self.previous
+        self.previous = self.end
+        return (self.delta, self.interval)
 
-results = dict()
-results['shgo_sobol'] = opt.shgo(partial(logged_function, locals()[function_name]), bounds, sampling_method='sobol')
 
-minima_list_inputs = results['shgo_sobol'].xl
-minima_list_outputs = results['shgo_sobol'].funl
-global_minimum_location = results['shgo_sobol'].x
-global_minimum = results['shgo_sobol'].fun
+if __name__ == '__main__':
+    multiprocessing.freeze_support()  # For Windows compatibility
+    bounds = [(-512, 512), (-512, 512)]
+    max_threads = 3  # Number of workers for parallelization
+    num_sampling_points = 100  # Number of evaluations
+    use_default_num_points = True  # Use default number of points for each method
 
-function_evaluations = results['shgo_sobol'].nfev
+    # optimization_method = 'shgo_simplicial'  # Uncomment to use shgo with simplicial sampling
+    # optimization_method = 'shgo_sobol'  # Uncomment to use shgo with Sobol sampling
+    optimization_method = 'differential_evolution'  # Uncomment to use differential evolution
+    # optimization_method = 'dual_annealing'  # Uncomment to use dual annealing
+    # optimization_method = 'basinhopping'  # Uncomment to use basinhopping
+
+
+    results = dict()
+    t = get_time()
+    if optimization_method == 'shgo_sobol':
+        print('Running SHGO with Sobol sampling...')
+        if use_default_num_points:
+            results = opt.shgo(myfunc, bounds, sampling_method='sobol', workers=max_threads)
+        else:
+            results = opt.shgo(myfunc, bounds, n=num_sampling_points, sampling_method='sobol', workers=max_threads)
+    elif optimization_method == 'shgo_simplicial':
+        print('Running SHGO with regular sampling...')
+        if use_default_num_points:
+            results = opt.shgo(myfunc, bounds, sampling_method='shgo_simplicial', workers=max_threads)
+        else:
+            results = opt.shgo(myfunc, bounds, n=num_sampling_points, sampling_method='shgo_simplicial', workers=max_threads)
+    elif optimization_method == 'differential_evolution':
+        print('Running Differential Evolution...')
+        if use_default_num_points:
+            results = opt.differential_evolution(myfunc, bounds, workers=max_threads, updating='deferred')
+        else:
+            results = opt.differential_evolution(myfunc, bounds, maxfun=num_sampling_points, workers=max_threads, updating='deferred')
+    elif optimization_method == 'dual_annealing':
+        print('Running Dual Annealing (workers is not supported)...')
+        results = opt.dual_annealing(myfunc, bounds)
+    print('Elapsed time:', t.delta())
+
+    function_evaluations = results['shgo_sobol'].nfev
 
 x = 2
