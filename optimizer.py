3a08984d5de400d2a82336477666d37862e225f7
Tislander1 <blurryprojection@gmail.com>
2025-04-17 22:56:12 -0600

    Collect all function evaluations into a list.

--- optimizer.py
+++ optimizer.py
@@ -1,17 +1,23 @@
-import scipy.optimize as opt
+
 import numpy as np
-from functools import partial
-from time import sleep, time
 import multiprocessing
+from time import sleep, time
+import scipy.optimize as opt
+from functools import partial
 
 
-def myfunc(vec):
+
+
+def myfunc(vec, *args):
     sleep(0.2) #Simulate a time-consuming function
     # print(f"Evaluating {vec}")
-    return (-(vec[1] + 47.0)
+    ans = (-(vec[1] + 47.0)
             * np.sin(np.sqrt(abs(vec[0]/2.0 + (vec[1] + 47.0))))
             - vec[0] * np.sin(np.sqrt(abs(vec[0] - (vec[1] + 47.0))))
             )
+    args[0].append({'in': vec, 'out': ans})  # Append the evaluated vector to the shared list
+    print(args[1])
+    return ans
 
 class get_time:
     """A context manager to measure the execution time of a block of code."""
@@ -40,39 +46,45 @@ if __name__ == '__main__':
     # optimization_method = 'dual_annealing'  # Uncomment to use dual annealing
     # optimization_method = 'basinhopping'  # Uncomment to use basinhopping
 
+    executable = 'myprogram.exe'  # Replace with your executable name
 
-    results = dict()
-    t = get_time()
-    if optimization_method == 'shgo_sobol':
-        print('Running SHGO with Sobol sampling...')
-        if use_default_num_points:
-            results = opt.shgo(myfunc, bounds, sampling_method='sobol', workers=max_threads)
-        else:
-            results = opt.shgo(myfunc, bounds, n=num_sampling_points, sampling_method='sobol', workers=max_threads)
-    elif optimization_method == 'shgo_simplicial':
-        print('Running SHGO with regular sampling...')
-        if use_default_num_points:
-            results = opt.shgo(myfunc, bounds, sampling_method='shgo_simplicial', workers=max_threads)
-        else:
-            results = opt.shgo(myfunc, bounds, n=num_sampling_points, sampling_method='shgo_simplicial', workers=max_threads)
-    elif optimization_method == 'differential_evolution':
-        print('Running Differential Evolution...')
-        if use_default_num_points:
-            results = opt.differential_evolution(myfunc, bounds, workers=max_threads, updating='deferred')
-        else:
-            results = opt.differential_evolution(myfunc, bounds, maxfun=num_sampling_points, workers=max_threads, updating='deferred')
-    elif optimization_method == 'dual_annealing':
-        print('Running Dual Annealing (workers is not supported)...')
-        results = opt.dual_annealing(myfunc, bounds)
-    print('Elapsed time:', t.delta())
+    with multiprocessing.Manager() as manager:  # Create a manager
+        global lst
+        lst = manager.list()  # Create a synchronized list
 
-    function_evaluations = results.nfev
-    print('Function evaluations:', function_evaluations)
-    print('Global optimum input:', results.x)
-    print('Global optimum output:', results.fun)
-    if 'xl' in results:
-        print('Local optimum inputs:', results.xl)
-    if 'funl' in results:
-        print('Local optimum outputs:', results.funl)
+        results = dict()
+        t = get_time()
+        if optimization_method == 'shgo_sobol':
+            print('Running SHGO with Sobol sampling...')
+            if use_default_num_points:
+                results = opt.shgo(myfunc, args = (lst, executable),  bounds=bounds, sampling_method='sobol', workers=max_threads)
+            else:
+                results = opt.shgo(myfunc, args = (lst, executable), bounds=bounds, n=num_sampling_points, sampling_method='sobol', workers=max_threads)
+        elif optimization_method == 'shgo_simplicial':
+            print('Running SHGO with regular sampling...')
+            if use_default_num_points:
+                results = opt.shgo(myfunc, args = (lst, executable), bounds=bounds, sampling_method='shgo_simplicial', workers=max_threads)
+            else:
+                results = opt.shgo(myfunc, args = (lst, executable), bounds=bounds, n=num_sampling_points, sampling_method='shgo_simplicial', workers=max_threads)
+        elif optimization_method == 'differential_evolution':
+            print('Running Differential Evolution...')
+            if use_default_num_points:
+                results = opt.differential_evolution(myfunc, args = (lst, executable), bounds=bounds, workers=max_threads, updating='deferred')
+            else:
+                results = opt.differential_evolution(myfunc, args = (lst, executable), bounds=bounds, maxfun=num_sampling_points, workers=max_threads, updating='deferred')
+        elif optimization_method == 'dual_annealing':
+            print('Running Dual Annealing (workers is not supported)...')
+            results = opt.dual_annealing(myfunc, args = (lst, executable), bounds=bounds)
+        print('Elapsed time:', t.delta())
+
+        function_evaluations = results.nfev
+        print('Function evaluations:', function_evaluations)
+        print('Global optimum input:', results.x)
+        print('Global optimum output:', results.fun)
+        if 'xl' in results:
+            print('Local optimum inputs:', results.xl)
+        if 'funl' in results:
+            print('Local optimum outputs:', results.funl)
+        print(lst)
 
 x = 2
